049f73bf3442d45f74cd014c78f98c1a6cebc56b
mungai1071 <1071johnmungai@gmail.com>
2024-11-07 20:58:42 +0300

    Update CAT2_Q2.c

--- CAT2_Q2.c
+++ CAT2_Q2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Declare and initialize a 3D array to represent two 2D arrays
+   
     int scores[2][2][2] = {
         {{65, 92}, {84, 72}},  // First set
         {{35, 70}, {59, 67}}   // Second set
     };
 
-    // Display the 3D array values
+   
     printf("Scores:\n");
     for (int i = 0; i < 2; i++) {
         printf("Set %d:\n", i + 1);
