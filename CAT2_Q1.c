53522947435338ced89a178de49f411c31a8c5c3
mungai1071 <1071johnmungai@gmail.com>
2024-11-07 20:58:11 +0300

    Update CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 
 struct Employee {
-    char name[25];
+    char name[25],department[20],email[50];
     int id;
-    char department[20];
     float salary;
-    char email[50];
-};
+
+}Employee;
 
 int main() {
-    // Declare and initialize an Employee structure variable with specified data
-    struct Employee emplo = {
-        "John Doe",             // name
-        12345,                  // id
-        "Human resources",      // department
-        55000.50,               // salary
-        "johndoe@company.com"   // email
-    };
+  strcpy(Employee.name,"John Doe");
+  strcpy(Employee.department,"Human Resources");
+  strcpy(Employee.email,"john.doe@company.com");
+  Employee.salary =55000.50;
+  Employee.id = 12345;
+
+    
 
     // Display the employee information
-    printf("Employee Information:\n");
-    printf("Name: %s\n", emplo.name);
-    printf("ID: %d\n", emplo.id);
-    printf("Department: %s\n", emplo.department);
-    printf("Salary: %.2f\n", emplo.salary);
-    printf("Email: %s\n", emplo.email);
+    printf("your name:%s\n", Employee.name);
+    printf("Department: %s\n",Employee.department );
+    printf("ID: %d\n", employee.id);
+    printf("Salary: %.2f\n", Employee.salary);
+    printf("Email: %s\n", Employee.email);
 
     return 0;
 }
