d716c2ca30a12fe24ca7139a8ecba610af39981c
mungai1071 <1071johnmungai@gmail.com>
2024-11-09 20:01:38 +0300

    Update CAT2_Q2.c

--- CAT2_Q2.c
+++ CAT2_Q2.c
@@ -1,23 +1,27 @@
+// 2D array
 #include <stdio.h>
 
 int main() {
-   
-    int scores[2][2][2] = {
-        {{65, 92}, {84, 72}},  // First set
-        {{35, 70}, {59, 67}}   // Second set
+   //printf(), scanf()
+     // Declare and initialize a 2D arrays
+   int i;
+   int j;
+    int scores[2][2] = {
+        {{65, 92}, {84, 72}},  
+        {{35, 70}, {59, 67}}   
     };
 
-   
+   //Display 3 D arrays values
     printf("Scores:\n");
     for (int i = 0; i < 2; i++) {
-        printf("Set %d:\n", i + 1);
+        printf("Set%d \n", i + 1);
         for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                printf("%d ", scores[i][j][k]);
+             
+                printf(" score[%d][%d]=%d \n ", i,j, scores[i][j]);
             }
-            printf("\n");
-        }
-        printf("\n");
+            
+        
+        
     }
 
     return 0;
